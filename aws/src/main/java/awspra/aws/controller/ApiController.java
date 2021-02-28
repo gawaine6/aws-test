738623303893fdef7bf4e3a32e3ae7684c9ccfb9
gawaine6 <gawaine6@gmail.com>
2021-02-28 14:59:39 +0900

    메인 이것저것(수정)

--- aws/src/main/java/awspra/aws/controller/ApiController.java
+++ aws/src/main/java/awspra/aws/controller/ApiController.java
@@ -9,22 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class ApiController {
 
-    private BoardController boardController;
-    private MemberController memberController;
-
     @GetMapping("/")
     public String main() {
         return "main";
     }
-
-    @GetMapping("/board")
-    public String board() {
-        return "/board/list.html";
-    }
-
-    @GetMapping("/member")
-    public String member_list() {
-        return "/member/member_list.html";
-    }
-
 }
