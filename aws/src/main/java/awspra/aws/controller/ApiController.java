7e61a2438731e17f98bf16f20245ac5ece265f0d
gawaine6 <gawaine6@gmail.com>
2021-02-28 14:54:23 +0900

    회원가입 씨바라

---
+++ aws/src/main/java/awspra/aws/controller/ApiController.java
@@ -0,0 +1,15 @@
+package awspra.aws.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+
+public class ApiController {
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
