df1b1a3e1bc06bf407490046b351accbce84e7e1
gawaine6 <50446718+gawaine6@users.noreply.github.com>
2021-03-11 14:40:47 +0900

    Board 들어간거 전부 주석처리했음 디비에 올리는데 타입에러 빌드, 어플 프로퍼티 오라클로 수정

--- aws/src/main/java/awspra/aws/controller/BoardController.java
+++ aws/src/main/java/awspra/aws/controller/BoardController.java
@@ -1,39 +1,39 @@
-package awspra.aws.controller;
-
-import awspra.aws.dto.BoardDto;
-import awspra.aws.service.BoardService;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
-
-@Controller
-// 컨트롤러 명시, RestController도 있는데 controller + responsebody
-@AllArgsConstructor
-// 클래스에 존재하는 모든 필드에 대한 생성자 자동 생성
-public class BoardController {
-
-    private BoardService boardService;
-
-    @GetMapping("/board")
-    public String list(Model model) {
-        List<BoardDto> boardList = boardService.getBoardList();
-        model.addAttribute("boardList", boardList);
-        return "board/list.html";
-    }
-
-    @GetMapping("/post")
-    public String write() {
-        return "board/write.html";
-    }
-
-    @PostMapping("/post")
-    public String write(BoardDto boardDto) {
-        boardService.savePost(boardDto);
-
-        return "redirect:/board";
-    }
-}
+//package awspra.aws.controller;
+//
+//import awspra.aws.dto.BoardDto;
+//import awspra.aws.service.BoardService;
+//import lombok.AllArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//import java.util.List;
+//
+//@Controller
+//// 컨트롤러 명시, RestController도 있는데 controller + responsebody
+//@AllArgsConstructor
+//// 클래스에 존재하는 모든 필드에 대한 생성자 자동 생성
+//public class BoardController {
+//
+//    private BoardService boardService;
+//
+//    @GetMapping("/board")
+//    public String list(Model model) {
+//        List<BoardDto> boardList = boardService.getBoardList();
+//        model.addAttribute("boardList", boardList);
+//        return "board/list.html";
+//    }
+//
+//    @GetMapping("/post")
+//    public String write() {
+//        return "board/write.html";
+//    }
+//
+//    @PostMapping("/post")
+//    public String write(BoardDto boardDto) {
+//        boardService.savePost(boardDto);
+//
+//        return "redirect:/board";
+//    }
+//}
--- aws/src/main/java/awspra/aws/domain/entity/MemberEntity.java
+++ aws/src/main/java/awspra/aws/domain/entity/MemberEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class MemberEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long memberNo;
 
     @Column(length = 30, nullable = false)
